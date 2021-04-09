4311b3a054bb31a50c97cbc3f94aa2baa6d0bb9e
Ahmad Hussain <63265101+Ahmad-Hussain-97@users.noreply.github.com>
2021-04-09 22:21:05 +0500

    Hello react app! using arg.para and arg.title

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function H1(arg) {
-  return <h1>{arg.title}</h1>;
+  return (
+    <div>
+      <h1>{arg.title}</h1>
+      <p>{arg.para}</p>
+    </div>
+  );
 }
 
 ReactDOM.render(
   <div>
-    <H1 title="Hello React App!" />
+    <H1
+      title="Hello React App!"
+      para="This is the paragraph to be shown in p tag"
+    />
   </div>,
   document.getElementById("root")
 );
