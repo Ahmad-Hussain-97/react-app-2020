f808e22e8ce9fb9485bf77154c897eda636a74c8
Ahmad Hussain <63265101+Ahmad-Hussain-97@users.noreply.github.com>
2021-04-09 22:09:46 +0500

    Multiple react functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function H1() {
-  return <div>Hello World</div>;
+  return <div>Hello World 1</div>;
 }
-ReactDOM.render(<H1 />, document.getElementById("root"));
+function H2() {
+  return <div>Hello World 2</div>;
+}
+function H3() {
+  return <div>Hello World 3</div>;
+}
+
+function H4() {
+  return <div>Hello World 4</div>;
+}
+function H5() {
+  return <div>Hello World 5</div>;
+}
+
+ReactDOM.render(
+  <div>
+    <H1 />
+    <H2 />
+    <H3 />
+    <H4 />
+    <H5 />
+  </div>,
+  document.getElementById("root")
+);
