d3823ffab27d5ce66c85e5dfe3dbb89c7cee6e26
Ahmad Hussain <63265101+Ahmad-Hussain-97@users.noreply.github.com>
2021-04-09 22:04:05 +0500

    H1 function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+function H1() {
+  return <div>Hello World</div>;
+}
+ReactDOM.render(<H1 />, document.getElementById("root"));
